1fc9839e7e4cdb9ea49dc964339ac1cb503d873d
Ajdin <aydinholjan@gmail.com>
2025-01-19 23:22:04 +0100

    Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@
  */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || process.env.VERCEL_URL || 'http://localhost:3000',
+  siteUrl: process.env.SITE_URL || process.env.VERCEL_URL || 'https://ajdinholjan.com/',
   generateRobotsTxt: true,
   // ...other options
 }
